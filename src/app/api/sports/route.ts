b8ef0c1d61bffc7ba7fa65c4881870b5ac8fc82d
Wizardzaron <ryan013192@hotmail.com>
2025-09-16 08:26:09 -0700

    Hodaris other changes

---
+++ src/app/api/sports/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import { db } from '@/lib/db';
+import { sports } from '@/lib/schema';
+import { asc } from 'drizzle-orm';
+
+export async function GET() {
+  try {
+    const sport = await db
+      .select({
+        id: sports.id,
+        name: sports.name,
+      })
+      .from(sports)
+      .orderBy(asc(sports.display_order));
+
+    // console.log("SPORT: ", sport);
+    
+
+    return NextResponse.json({
+        sport
+    });
+  } catch (error) {
+    console.error('Error fetching sports:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
