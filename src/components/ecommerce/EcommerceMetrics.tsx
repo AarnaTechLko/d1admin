d7d2196c93cc9905874ca02a14fb8525e80a221a
Wizardzaron <ryan013192@hotmail.com>
2025-09-16 11:08:40 -0700

    Made the boxes bigger

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -130,12 +130,12 @@ export const EcommerceMetrics = () => {
     <div className="flex justify-between grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 w-[1000px]">
 
       {/* <!-- Metric Item Start --> */}
-      <div className=" rounded-2xl border border-gray-200 bg-sky-100 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6" >
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
+      <Link href="/coach">
+        <div className=" rounded-2xl border border-gray-200 bg-sky-100 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6" >
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
 
-        <Link href="/coach">
           <div className="flex items-end justify-between mt-5">
 
             <div>
@@ -152,39 +152,42 @@ export const EcommerceMetrics = () => {
               11.01%
             </Badge> */}
           </div>
-        </Link>
+        </div>
+      </Link>
 
-      </div>
+
       {/* <!-- Metric Item End --> */}
 
       {/* <!-- Metric Item Start --> */}
-      <div className="rounded-2xl border border-gray-200 bg-green-200 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-        <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
-
-        <Link href="/player">
-          <div className="flex items-end justify-between mt-5">
-
-              <div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Players
-                </span>
-                <h4 className="mt-2 font-bold text-gray-700 text-xl dark:text-white/90">
-                {players}
-                </h4>
-              </div>          
-          </div>
-        </Link>
-      </div>
-      {/* extra add column player/coches */}
-      <div className="rounded-2xl border border-gray-200 bg-red-100 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6 " >
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+      <Link href="/player">
+        <div className="rounded-2xl border border-gray-200 bg-green-200 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6">
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
           <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
+
+            <div className="flex items-end justify-between mt-5">
+
+                <div>
+                  <span className="text-sm text-gray-500 dark:text-gray-400">
+                    Players
+                  </span>
+                  <h4 className="mt-2 font-bold text-gray-700 text-xl dark:text-white/90">
+                  {players}
+                  </h4>
+                </div>          
+            </div>
         </div>
+      </Link>
 
 
-        <Link href="/organization">
+      {/* extra add column player/coches */}
+
+      <Link href="/organization">
+        <div className="rounded-2xl border border-gray-200 bg-red-100 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6 " >
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+          </div>
+
           <div className="flex items-end justify-between mt-5">
             <div>
               <span className="text-sm text-gray-500 dark:text-gray-400">
@@ -196,34 +199,35 @@ export const EcommerceMetrics = () => {
             </div>
           
           </div>
-        </Link>
+        </div>
+      </Link>
 
-      </div>
+
       {/* <!-- Metric Item End --> */}
 
       {/* <!-- Metric Item Start --> */}
-
-      <div className="rounded-2xl border border-gray-200 bg-blue-200 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6 " >
-        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
-        </div>
-
-        <a href="/team">
-          <div className="flex items-end justify-between mt-5">
-            <div>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                Teams
-              </span>
-              <h4 className="mt-2 font-bold text-gray-700 text-xl dark:text-white/90">
-              {totalCount}
-              </h4>
-            </div>
-            
+      <Link href="/team">
+        <div className="rounded-2xl border border-gray-200 bg-blue-200 p-4 dark:border-gray-800 dark:bg-sky-900 md:p-6 " >
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
           </div>
-        </a>
 
-      </div>
-      
+          <a href="/team">
+            <div className="flex items-end justify-between mt-5">
+              <div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Teams
+                </span>
+                <h4 className="mt-2 font-bold text-gray-700 text-xl dark:text-white/90">
+                {totalCount}
+                </h4>
+              </div>
+              
+            </div>
+          </a>
+
+        </div>
+      </Link>
       {/* <!-- Metric Item End --> */}
     </div>
   );
